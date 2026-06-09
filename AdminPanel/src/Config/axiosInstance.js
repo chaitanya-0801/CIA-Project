b647429afba3afc56541a33d17733616b88a9e9c
Chaitanya <csbd06@gmail.com>
2026-06-09 23:45:41 +0530

    base url updated

--- AdminPanel/src/Config/axiosInstance.js
+++ AdminPanel/src/Config/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
-    import.meta.env.VITE_API_BASE_URL || import.meta.env.VITE_API_BASE_URL_2,
+    import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
   },
--- Frontend/src/Config/axiosInstance.js
+++ Frontend/src/Config/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_BASE_URL || import.meta.env.VITE_API_BASE_URL_2,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     "Content-Type": "multipart/form-data",
   },
