c3fc75c4eeb9f4086c81534469ae3a015a7f4363
Chaitanya <csbd06@gmail.com>
2026-06-14 11:41:50 +0530

    Data change

--- Frontend/src/Data/OurTeam.js
+++ Frontend/src/Data/OurTeam.js
@@ -6,7 +6,7 @@ const OurTeamData = [
     name: "Sandeep Chaudhary",
     role: "Director",
     image: Director,
-    contact: "+91 98765 43210",
+    contact: "+91 82952 80143",
     email: "cialadwa@gmail.com",
     message:
       "At Chaudhary Immigration Academy, our goal is to guide students and professionals toward successful international opportunities. We are committed to providing honest advice, personalized counseling, and reliable visa assistance to make your journey abroad smooth and stress-free.",
@@ -15,8 +15,8 @@ const OurTeamData = [
     name: "Jyoti Devi",
     role: "Senior Consultant",
     image: Team1,
-    contact: "+91 98765 43211",
-    email: "jyoti@cia.com",
+    contact: "+91 80530 21056",
+    email: "jyotibanait13@gmail.com",
     message:
       "We provide personalized counseling and expert support to help students choose the right universities, courses, and visa pathways for their future success.",
   },
